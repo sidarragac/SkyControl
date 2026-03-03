169b2ef1e4533445c4c3a629dbb8a3c26ad6b20e
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-03-03 16:17:35 -0500

    feat(DisplayDataComponent): add new component for displaying information

---
+++ frontend/src/components/DisplayDataComponent.vue
@@ -0,0 +1,21 @@
+<!-- Developed by Santiago Idárraga -->
+<script setup lang="ts">
+defineProps<{
+  mainText: string;
+  secondaryText: string;
+  imageURL: string;
+}>();
+
+</script>
+
+<template>
+  <div class="flex items-center gap-3">
+    <div class="w-12 h-12 rounded-lg bg-primary-900/10 flex items-center justify-center overflow-hidden">
+      <img :src="imageURL" :alt="mainText" class="w-full h-full object-cover">
+    </div>
+    <div>
+      <p class="font-semibold text-sm">{{ mainText }}</p>
+      <p class="text-xs text-slate-500">{{ secondaryText }}</p>
+    </div>
+  </div>
+</template>
