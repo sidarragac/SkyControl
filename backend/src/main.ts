11001bf492a7b3f03997794e330111a6c1cb674f
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-04-18 20:32:10 -0500

    feat: validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 // External imports
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 // Internal imports
 import { AppModule } from './app.module';
@@ -12,6 +13,14 @@ async function bootstrap() {
   });
 
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
