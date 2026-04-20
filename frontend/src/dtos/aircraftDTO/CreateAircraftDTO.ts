54f7342e7a96623f35d2b01e13d5888534675bd8
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-04-20 18:06:08 -0500

    refactor: remove unused conditional

--- frontend/src/dtos/aircraftDTO/CreateAircraftDTO.ts
+++ frontend/src/dtos/aircraftDTO/CreateAircraftDTO.ts
@@ -3,6 +3,6 @@
 import type { AircraftInterface } from '@/interfaces/AircraftInterface.js';
 
 export type CreateAircraftDTO = Omit<AircraftInterface, 'id' | 'airline' | 'manufacturer' | 'createdAt' | 'updatedAt'> & {
-  airlineId?: string;
+  airlineId: string;
   manufacturerId: string;
 };
